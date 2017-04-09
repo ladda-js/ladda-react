d0dede6b33cd6e6aa554ae1dc2a40553bd50d1c2
LFDM <1986gh@gmail.com>
2017-04-09 18:03:04 +0200

    Make suscription test pass

--- src/hocs/withResolve/index.js
+++ src/hocs/withResolve/index.js
@@ -1,18 +1,13 @@
 import { createElement, Component } from 'react';
 
-const zip = (xs, ys) => {
-  const toTake = Math.min(xs.length, ys.length);
-  const zs = [];
-  for (let i = 0; i < toTake; i++) {
-    zs.push([xs[i], ys[i]]);
-  }
-  return zs;
-};
-
-
 class Container extends Component {
   constructor(props) {
     super(props);
+
+    this.resolvedData = {};
+    this.resolvedDataTargetSize = 0;
+
+    this.subscribers = [];
 
     this.state = {
       pending: false,
@@ -21,32 +16,66 @@ class Container extends Component {
     };
   }
 
+  unsubscribe() {
+    while (this.subscribers.length) {
+      this.subscribers.pop().destroy();
+    }
+  }
+
   componentWillMount() {
     this.trigger(this.props);
   }
 
   componentWillReceiveProps(newProps) {
+    this.unsubscribe();
     this.trigger(newProps);
+  }
+
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
+
+  addResolvedData(field, data) {
+    this.resolvedData[field] = data;
+    if (this.resolvedDataTargetSize === Object.keys(this.resolvedData).length) {
+      this.setState({
+        pending: false,
+        resolvedProps: { ...this.resolvedData },
+        error: null
+      });
+    }
+  }
+
+  setError(error) {
+    this.setState({ pending: false, error });
   }
 
   trigger(props) {
     this.setState({ pending: true, error: null });
-    const { resolve = {}, originalProps } = props;
-    const asList = Object.keys(resolve).map((key) => [key, resolve[key]]);
-    const promises = Promise.all(asList.map(el => el[1](originalProps)));
-    promises.then(
-      (results) => {
-        const resolvedProps = zip(asList, results).reduce(
-          (mem, el) => {
-            mem[el[0][0]] = el[1];
-            return mem;
-          },
-          {}
-        );
-        this.setState({ pending: false, resolvedProps, error: null });
-      },
-      error => this.setState({ pending: false, error })
-    );
+    const { resolve = {}, subscribe = {}, originalProps } = props;
+    const resolveKeys = Object.keys(resolve);
+    const subscribeKeys = Object.keys(subscribe);
+
+    this.resolvedDataTargetSize = resolveKeys.length + subscribeKeys.length;
+
+    resolveKeys.forEach((key) => {
+      const promise = resolve[key](originalProps);
+      // make sure we have a promise, otherwise throw with a clear message
+      promise.then(
+        (data) => this.addResolvedData(key, data),
+        (err) => this.setError(err)
+      );
+    });
+
+    this.subscribers = subscribeKeys.map((key) => {
+      const subscriber = subscribe[key](originalProps);
+      // validate subscriber, throw meaningful error otherwise
+      subscriber.subscribe(
+        (data) => this.addResolvedData(key, data),
+        (err) => this.setError(err)
+      );
+      return subscriber;
+    });
   }
 
   render() {
